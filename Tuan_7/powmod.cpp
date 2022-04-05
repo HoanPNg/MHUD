e8cc2923ae00dc3d5a8585d007fcbbd2d3882e5f
Hoan Phan <hiiamhoan@gmail.com>
2022-04-05 17:04:02 +0700

    add BT Tuan 7

---
+++ Tuan_7/powmod.cpp
@@ -0,0 +1,83 @@
+#include<iostream>
+#include <cstdlib> 
+#include <ctime>
+
+
+using namespace std;
+
+unsigned __int64 PowerMod(unsigned __int64 x, unsigned __int64 b, unsigned __int64 n) {
+	unsigned __int64 r = 1;
+	while (b > 0)
+	{
+		x = x % n;
+		if (b & 1 == 1)
+		{
+			r = (r * x) % n;
+			
+		}
+		x = (x * x) % n;
+		b = b >> 1;
+	}
+	return r;
+}
+
+bool miller_rabin(unsigned __int64 n)
+{
+	unsigned __int64 n_temp = n - 1; 
+	int m,k = 0;
+	while(n_temp > 0)
+	{
+		if(n_temp & 1 == 1)
+			{
+				m = n_temp;
+				break;
+			}
+		k++;
+		n_temp >>= 1;
+	}
+	int ran = 2 + rand() % (n-3);
+	
+	if (n >= 50)
+	{
+		ran = 2 + ran % 49; 
+	}
+
+	cout << "ran =  " << ran << endl;
+
+	unsigned __int64 temp = PowerMod(ran,m,n);
+
+	if(temp == 1|| temp == n-1)
+		return true;
+	
+	for(int i = 1; i < k; i++)
+	{
+		temp = PowerMod(temp,2,n);
+		if (temp == n-1)
+			return true;
+	}
+
+	return false;
+
+}
+ 
+bool checkPrime(unsigned __int64 n, int tries)
+{
+	for(int i = 0; i < tries ; i++)
+	{
+		if(!miller_rabin(n))
+			return false;
+	}
+
+	return true;
+
+}
+
+int main() {
+	srand((int)time(0));
+	unsigned __int64 n;
+	cout << "n = "; cin >>n;
+	if(checkPrime(n,5))
+		cout << "prime";
+	else
+		cout << "not prime";
+}
